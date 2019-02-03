24df072fb86ae2687321d8dff89b53d941927f05
tails5555 <tails5555@naver.com>
2019-02-03 23:28:31 +0900

    Create AJAX Mechanism, Promise Docs ajax 와 Web 의 동작 과정 및 Promise 패턴에 대한 스터디 노트 작성 완료.

--- src/App.js
+++ src/App.js
@@ -4,36 +4,14 @@ import Movie from './Movie';
 
 class App extends PureComponent {
   state = {
-    
+
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title : '동작그만',
-            poster : 'https://i.ytimg.com/vi/zwH4-h7hx-k/hqdefault.jpg'
-          },
-          {
-            title : '짱구는 못말려',
-            poster : 'http://image.chosun.com/sitedata/image/201706/23/2017062302105_0.jpg'
-          },
-          {
-            title : '고등래퍼',
-            poster : 'http://static.global.mnet.com/data/od/images/schoolrapper2/premiere/mobile/m_main.jpg'
-          },
-          {
-            title : '거침없이 하이킥',
-            poster : 'http://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2016/05/12/a2e2557a-12ac-4d04-9811-0c3fe3eaa404.jpg' 
-          },
-          {
-            title : '헬스 키친',
-            poster : 'https://ncache.ilbe.com/files/attach/new/20150610/1638661929/870153271/5978982068/738414ae486f31994424f31a932bd2ea.jpg'
-          }
-        ]
-      })
-    }, 5000);
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(err => console.log(err));
   }
 
   _renderMovies = () => {
